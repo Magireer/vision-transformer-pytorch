8e0bc04e2de3d764fcffecd43df2582dbaf5c995
Marcus Reed <vituctelustcrib@gmx.net>
2023-03-15 00:00:00 -0400

    Initial commit: Project structure and basic ViT architecture

--- vit.py
+++ vit.py
@@ -1,24 +1,25 @@
 import torch
 import torch.nn as nn
+from typing import Optional, Tuple
 
 class PatchEmbedding(nn.Module):
     """Splits image into patches and embeds them."""
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+    def __init__(self, img_size: int = 224, patch_size: int = 16, in_chans: int = 3, embed_dim: int = 768):
         super().__init__()
         self.img_size = img_size
         self.patch_size = patch_size
         self.n_patches = (img_size // patch_size) ** 2
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
-    def forward(self, x):
-        x = self.proj(x)  # (n_samples, embed_dim, n_patches**0.5, n_patches**0.5)
-        x = x.flatten(2)  # (n_samples, embed_dim, n_patches)
-        x = x.transpose(1, 2)  # (n_samples, n_patches, embed_dim)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.proj(x)  # (B, E, H/P, W/P)
+        x = x.flatten(2)  # (B, E, N)
+        x = x.transpose(1, 2)  # (B, N, E)
         return x
 
-class Attention(nn.Module):
-    """Multi-head self-attention mechanism."""
-    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+class MultiHeadAttention(nn.Module):
+    """Multi-head self-attention mechanism with optimized scaling."""
+    def __init__(self, dim: int, n_heads: int = 12, qkv_bias: bool = True, attn_p: float = 0., proj_p: float = 0.):
         super().__init__()
         self.n_heads = n_heads
         self.dim = dim
@@ -29,73 +30,59 @@ class Attention(nn.Module):
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_p)
 
-    def forward(self, x):
-        n_samples, n_tokens, dim = x.shape
-        qkv = self.qkv(x).reshape(n_samples, n_tokens, 3, self.n_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.n_heads, self.head_dim).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]
-        k_t = k.transpose(-2, -1)
-        dp = (q @ k_t) * self.scale
-        attn = dp.softmax(dim=-1)
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
-        weighted_avg = attn @ v
-        weighted_avg = weighted_avg.transpose(1, 2).reshape(n_samples, n_tokens, dim)
-        x = self.proj(weighted_avg)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
         x = self.proj_drop(x)
         return x
 
-class MLP(nn.Module):
-    """Multilayer perceptron."""
-    def __init__(self, in_features, hidden_features, out_features, p=0.):
-        super().__init__()
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = nn.GELU()
-        self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(p)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
-class Block(nn.Module):
-    """Transformer block."""
-    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0., attn_p=0.):
+class TransformerBlock(nn.Module):
+    """Standard Transformer block with LayerNorm and MLP."""
+    def __init__(self, dim: int, n_heads: int, mlp_ratio: float = 4.0, p: float = 0., attn_p: float = 0.):
         super().__init__()
         self.norm1 = nn.LayerNorm(dim, eps=1e-6)
-        self.attn = Attention(dim, n_heads=n_heads, qkv_bias=qkv_bias, attn_p=attn_p, proj_p=p)
+        self.attn = MultiHeadAttention(dim, n_heads=n_heads, attn_p=attn_p, proj_p=p)
         self.norm2 = nn.LayerNorm(dim, eps=1e-6)
-        self.mlp = MLP(in_features=dim, hidden_features=int(dim * mlp_ratio), out_features=dim, p=p)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, int(dim * mlp_ratio)),
+            nn.GELU(),
+            nn.Dropout(p),
+            nn.Linear(int(dim * mlp_ratio), dim),
+            nn.Dropout(p)
+        )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x + self.attn(self.norm1(x))
         x = x + self.mlp(self.norm2(x))
         return x
 
 class VisionTransformer(nn.Module):
-    """Vision Transformer model."""
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, n_classes=1000, embed_dim=768, depth=12, n_heads=12, mlp_ratio=4., qkv_bias=True, p=0., attn_p=0.):
+    """Vision Transformer (ViT) implementation for image classification."""
+    def __init__(self, img_size: int = 224, patch_size: int = 16, in_chans: int = 3, n_classes: int = 1000, 
+                 embed_dim: int = 768, depth: int = 12, n_heads: int = 12, mlp_ratio: float = 4., p: float = 0.):
         super().__init__()
-        self.patch_embed = PatchEmbedding(img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        self.patch_embed = PatchEmbedding(img_size, patch_size, in_chans, embed_dim)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         self.pos_embed = nn.Parameter(torch.zeros(1, 1 + self.patch_embed.n_patches, embed_dim))
         self.pos_drop = nn.Dropout(p=p)
-        self.blocks = nn.ModuleList([Block(dim=embed_dim, n_heads=n_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, p=p, attn_p=attn_p) for _ in range(depth)])
+        self.blocks = nn.ModuleList([TransformerBlock(embed_dim, n_heads, mlp_ratio, p) for _ in range(depth)])
         self.norm = nn.LayerNorm(embed_dim, eps=1e-6)
         self.head = nn.Linear(embed_dim, n_classes)
 
-    def forward(self, x):
-        n_samples = x.shape[0]
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B = x.shape[0]
         x = self.patch_embed(x)
-        cls_token = self.cls_token.expand(n_samples, -1, -1)
+        cls_token = self.cls_token.expand(B, -1, -1)
         x = torch.cat((cls_token, x), dim=1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
         for block in self.blocks:
             x = block(x)
         x = self.norm(x)
-        cls_token_final = x[:, 0]
-        x = self.head(cls_token_final)
-        return x
+        return self.head(x[:, 0])
